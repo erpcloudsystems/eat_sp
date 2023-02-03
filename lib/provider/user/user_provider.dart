a7006cd6dbf1a308eb116a15350e2969f9fe97c1
Mohamed Mohamedy <80913778+mohamedmohamedy@users.noreply.github.com>
2023-02-04 00:40:14 +0200

    change key

--- lib/provider/user/user_provider.dart
+++ lib/provider/user/user_provider.dart
@@ -106,7 +106,7 @@ class UserProvider extends ChangeNotifier {
         url = url.replaceRange(url.length - 1, url.length, '');
       service.changeUrl(url); // to set baseUrl for all app
       final res = await service.login(
-          "method/ecs_mobile.api.login", {"usr": username, "pwd": password, "url": url});
+          "method/ecs_mobile.api.login", {"usr": username, "pwd": password});
 
       if (res != null &&
           res['message']['success_key'].toString().toLowerCase() == 'true') {
