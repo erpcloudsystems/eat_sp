74badb2fdf04c79a7fb08018f6d02ef00440261e
Mohamed Mohamedy <mohamedmohamedy146@gmail.com>
2023-04-18 10:55:44 +0200

    adding firebase files

--- ios/build/Pods.build/Debug-iphonesimulator/geocoding.build/DerivedSources/geocoding_vers.c
+++
@@ -1,5 +0,0 @@
- extern const unsigned char geocodingVersionString[];
- extern const double geocodingVersionNumber;
-
- const unsigned char geocodingVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:geocoding  PROJECT:Pods-1" "\n";
- const double geocodingVersionNumber __attribute__ ((used)) = (double)1.;
