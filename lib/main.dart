e60ba5c99a281651405d46558e7cf9781d11a154
Mohamed Mohamedy <mohamedmohamedy146@gmail.com>
2023-05-14 10:10:45 +0300

    edit on main

--- lib/main.dart
+++ lib/main.dart
@@ -7,7 +7,6 @@ import 'package:easy_localization/easy_localization.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
