2e1393afb3935eb3ba13a49150a0063bf229324c
Mohamed_Mohamedy <mohamedmohamedy146@gmail.com>
2023-08-24 12:26:45 +0300

    update version 2.1.0

--- lib/screen/drawer/aboutus_screen.dart
+++ lib/screen/drawer/aboutus_screen.dart
@@ -31,65 +31,63 @@ class _AboutUsState extends State<AboutUs> {
     double height = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      body: Container(
-        child: SingleChildScrollView(
-          physics: const BouncingScrollPhysics(),
-          child: SizedBox(
-            width: width,
-            height: height,
-            child: Padding(
-              padding: const EdgeInsets.all(20.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: <Widget>[
-                  const Text('NextApp',
-                      style:
-                          TextStyle(fontSize: 35, fontWeight: FontWeight.w600)),
-                  Container(
-                    width: 150,
-                    height: 2,
-                    color: Colors.orangeAccent,
-                  ),
-                  const SizedBox(height: 30),
-                  const Text('Version',
-                      style:
-                          TextStyle(fontSize: 16, fontWeight: FontWeight.w400)),
-                  Text(versionNumber,
-                      style: const TextStyle(
-                          fontSize: 18, fontWeight: FontWeight.w400)),
-                  const SizedBox(height: 30),
-                  const Text('Last Update',
-                      style:
-                          TextStyle(fontSize: 16, fontWeight: FontWeight.w400)),
-                  const Text('May 2023',
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.w400)),
-                  const SizedBox(height: 30),
-                  const Text(
-                      'NextApp App is cloud ERP System on mobile to make your work easier .',
-                      style:
-                          TextStyle(fontSize: 18, fontWeight: FontWeight.w400)),
-                  const SizedBox(height: 30),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      const Text('Powered by'),
-                      TextButton(
-                        onPressed: () {
-                          launchUrl(Uri.parse('https://www.erpcloud.systems/'));
-                        },
-                        child: const Text(
-                          'ERPCloud.systems',
-                          style: TextStyle(color: Colors.blue),
-                        ),
+      body: SingleChildScrollView(
+        physics: const BouncingScrollPhysics(),
+        child: SizedBox(
+          width: width,
+          height: height,
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                const Text('NextApp',
+                    style:
+                        TextStyle(fontSize: 35, fontWeight: FontWeight.w600)),
+                Container(
+                  width: 150,
+                  height: 2,
+                  color: Colors.orangeAccent,
+                ),
+                const SizedBox(height: 30),
+                const Text('Version',
+                    style:
+                        TextStyle(fontSize: 16, fontWeight: FontWeight.w400)),
+                Text(versionNumber,
+                    style: const TextStyle(
+                        fontSize: 18, fontWeight: FontWeight.w400)),
+                const SizedBox(height: 30),
+                const Text('Last Update',
+                    style:
+                        TextStyle(fontSize: 16, fontWeight: FontWeight.w400)),
+                const Text('Aug 2023',
+                    style:
+                        TextStyle(fontSize: 18, fontWeight: FontWeight.w400)),
+                const SizedBox(height: 30),
+                const Text(
+                    'NextApp App is cloud ERP System on mobile to make your work easier .',
+                    style:
+                        TextStyle(fontSize: 18, fontWeight: FontWeight.w400)),
+                const SizedBox(height: 30),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    const Text('Powered by'),
+                    TextButton(
+                      onPressed: () {
+                        launchUrl(Uri.parse('https://www.erpcloud.systems/'));
+                      },
+                      child: const Text(
+                        'ERPCloud.systems',
+                        style: TextStyle(color: Colors.blue),
                       ),
-                    ],
-                  ),
-                  const SizedBox(height: 30),
-                ],
-              ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 30),
+              ],
             ),
           ),
         ),
