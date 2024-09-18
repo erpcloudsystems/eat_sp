4d650623380d6aac33d29c6d1182b361dd20b2cf
Ayman <aymmanyehia23@gmail.com>
2024-09-18 11:29:37 +0300

    fix external storage permision

--- lib/new_version/modules/printer/controller/cubit/printer_cubit.dart
+++ lib/new_version/modules/printer/controller/cubit/printer_cubit.dart
@@ -184,7 +184,6 @@ class PrinterCubit extends Cubit<PrinterState> {
     if (!await requestStoragePermission()) {
       Fluttertoast.showToast(
           msg: "Permissions denied. Please grant Storage permissions.");
-      Navigator.pop(context);
       return;
     }
     try {
@@ -264,20 +263,33 @@ class PrinterCubit extends Cubit<PrinterState> {
 
   Future<bool> requestStoragePermission() async {
     if (Platform.isAndroid) {
-      if (await Permission.storage.isGranted) {
+      if (await Permission.storage.isGranted ||
+          (Platform.version.compareTo("30") >= 0 &&
+              await Permission.manageExternalStorage.isGranted)) {
+        // Permission is already granted for storage or manage external storage (Android 11+)
         return true;
       } else {
+        // For Android 10 and below, request normal storage permissions
         if (Platform.version.compareTo("30") < 0) {
           PermissionStatus status = await Permission.storage.request();
           return status == PermissionStatus.granted;
         } else {
+          // For Android 11 and above, request MANAGE_EXTERNAL_STORAGE
           PermissionStatus status =
               await Permission.manageExternalStorage.request();
-          return status == PermissionStatus.granted;
+          if (status == PermissionStatus.granted) {
+            return true;
+          } else if (status == PermissionStatus.denied) {
+            // If permission is denied, return false
+            return false;
+          } else if (status == PermissionStatus.permanentlyDenied) {
+            // If permission is permanently denied, open app settings
+            await openAppSettings();
+            return false;
+          }
         }
       }
-    } else {
-      return true;
     }
+    return true;
   }
 }
